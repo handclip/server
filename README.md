90deceb914d71baadcffb982ee8a13ae052684ea
Elon Gliksberg <elongliks@gmail.com>
2021-10-29 15:29:53 +0300

    Completed stage 1 of the algorithm.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Algorithm
 
-1. Receive video from the client.
-   1. Validate that the file is a video.
-   2. Store the video.
+1. Receive and store video from the client.
 2. Process video.
    1. Look for hand gestures (open --> closed --> open --> closed).
    2. Record the position in video.
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+import logging
+import uuid
+from os import path
+
+from fastapi import FastAPI, File, UploadFile
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+VIDEOS_DIR = 'videos'
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+async def save_video(file: UploadFile) -> str:
+    video_path = path.join(VIDEOS_DIR, uuid.uuid4())
+
+    with open(video_path, 'wb') as out_file:
+        while content := await file.read(1024):
+            out_file.write(content)
+
+    logger.info(f'Saved video to {video_path}')
+    return video_path
+
+
+@app.post('/upload/')
+async def upload_video(video_file: UploadFile = File(...)):
+    video_path = await save_video(video_file)
+
+    return {'filename': video_file.filename}
